047f1855a1f406702235042c100baefd97dd9e85
dadayama <dadayamism@gmail.com>
2021-03-02 17:27:57 +0900

    静的ファイルの配信

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.set('view engine', 'pug');
-
-app.use(require('./logger.js'));
+// ***** ルーティング *****
+// app.use(require('./logger.js'));
 // app.use('/users', require('./router/user.js'));
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', { title: 'Webアプリケーション開発' });
-})
+// ***** テンプレートエンジン ****
+// app.set('view engine', 'pug');
+// app.get('/', (req, res) => {
+//   res.status(200).render('index', { title: 'Webアプリケーション開発' });
+// })
+
+// ***** 静的ファイルの配信 *****
+app.use('/public', express.static(`${__dirname}/public`))
 
 app.listen(3000);
