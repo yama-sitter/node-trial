f5e06c47a882c9ee10677b415a1f8059e1005b64
dadayama <dadayamism@gmail.com>
2021-03-06 17:05:48 +0900

    body-parser導入

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.all('/', (req, res) => {
-  let body = '';
-  console.log(req.method);
-  console.log(req.url);
-  console.log(JSON.stringify(req.headers));
-  req.on('data', (chunk) => {
-    body += chunk;
-  });
-  req.on('end', () => {
-    console.log(body);
-  });
-  res.send('OK');
+app.set('view engine', 'pug')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('./index.pug');
 });
+app.post('/', (req, res) => {
+  res.send('OK');
+})
 
 app.listen(3000);
