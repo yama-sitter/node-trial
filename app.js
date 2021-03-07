497209d2ced24c06e30bd6880dedf7abfdf8c907
dadayama <dadayamism@gmail.com>
2021-03-07 23:12:18 +0900

    cookie情報取得・設定

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.set('view engine', 'pug')
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.set('view engine', 'pug');
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-  res.render('./index.pug');
+  const count = parseInt(req.cookies.count || 0);
+  res.cookie('count', count + 1);
+  res.render('./index.pug', { count });
 });
-app.post('/', (req, res) => {
-  res.send('OK');
-})
 
 app.listen(3000);
