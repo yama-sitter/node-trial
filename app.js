1817bb47059e444745bc251fab53aaef6ba0f2ea
dadayama <dadayamism@gmail.com>
2021-03-08 21:58:58 +0900

    セッション情報取得

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 
 app.set('view engine', 'pug');
-app.use(cookieParser());
+app.use(session({
+  secret: 'qwerty',
+  resave: false,
+  saveUninitialized: true,
+  name: 'sid',
+}));
 
 app.get('/', (req, res) => {
-  const count = parseInt(req.cookies.count || 0);
-  res.cookie('count', count + 1);
+  const count = req.session.count || 0;
+  req.session.count = count + 1;
   res.render('./index.pug', { count });
 });
 
